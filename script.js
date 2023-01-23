ec293a034a93fd6706faac0c86a7d3c9a87e25cf
yousef <yousef_salih@hotmail.com>
2023-01-23 15:17:57 +0300

    Fixing bug with the guess number and updating notes

--- script.js
+++ script.js
@@ -9,10 +9,10 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value); 
 let secretNumber = Math.trunc(Math.random() * 20 ) + 1 // #8 define secret number. We want it between 1 and 20. Math.random generates a decimal number and then multiplying it by 20 (between 1 and 20). The .trunc method makes the number without decimal (0 to 19) which is why we need to add the +1 to represent 1-20.
-document.querySelector('.number').textContent = secretNumber; //#8 update the DOM to the secret number
+// document.querySelector('.number').textContent = secretNumber; //#8 update the DOM to the secret number
 
 let score = 20; //#11 start the score
-let highscore = 0;
+let highscore = 0; //#21 initial highscore value
 
 document.querySelector('.check').addEventListener('click', function () { //Only called once event happens //#1create event listener
     const guess = Number(document.querySelector('.guess').value); //#2 create variable to store the value, #4 convert string to number
@@ -20,12 +20,13 @@ document.querySelector('.check').addEventListener('click', function () { //Only 
     if (!guess) { // #6 create if function to make sure the value is a number. If no value or not a number.
         document.querySelector('.message').textContent = '⛔️ No Number!';//#7 inform user
     } else if (guess === secretNumber){ //#9 check to see if the guess value is the same as the secret number. Player wins.
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.message').textContent = '🎉 Correct Number!'; //#10 update message
         document.querySelector('body').style.backgroundColor = '#60b347' // #18 Update CSS. If its two word CSS property name, you need to camel case.
         document.querySelector('.number').style.width = '30rem' //#19 update background width for the number element. CSS values always need to be in quotes (string).
-    if(score > highscore) {
+    if(score > highscore) { //#22 Store the highscore condition
         highscore = score;
-        document.querySelector('.highscore').textContent = highscore;
+        document.querySelector('.highscore').textContent = highscore; //#23 update highscore DOM
     }
 
     } else if (guess > secretNumber) { //When user guesses too high
@@ -50,7 +51,7 @@ document.querySelector('.check').addEventListener('click', function () { //Only 
     };
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector('.again').addEventListener('click', function () { //#20 Click again to reset everything
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = 'Start guessing...';
